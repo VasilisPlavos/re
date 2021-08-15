c81314590f1b481f5d0b12042d720f2928cccf90
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2021-08-16 02:40:26 +0300

    update homepage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Vasilis React App
+          Vasilis React App for Brain
         </p>
         <a
           className="App-link"
